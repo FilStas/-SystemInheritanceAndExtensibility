bf5918b3e36981d5cdf64d658f3ad4cc11b695cd
FilStas <filstas23@gmail.com>
2020-07-20 16:04:35 +0400

    added Book

---
+++ src/main/java/ru/netology/domain/Book.java
@@ -0,0 +1,26 @@
+package ru.netology.domain;
+
+public class Book extends Product {
+    private String author;
+
+    public Book() {
+        super();
+    }
+
+    public Book(String author) {
+        this.author = author;
+    }
+
+    public Book(int id, String name, int cost, String author) {
+        super(id, name, cost);
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
