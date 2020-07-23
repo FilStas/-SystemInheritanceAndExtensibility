c4b7b9250b97c9e4a4e070f221522739aa180136
FilStas <filstas23@gmail.com>
2020-07-23 18:25:53 +0400

    added ProductManagerTest

---
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -0,0 +1,56 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
+import ru.netology.repository.ProductRepository;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class ProductManagerTest {
+    @Mock
+    private ProductRepository repository;
+
+    @InjectMocks
+    private ProductManager manager;
+    private Book first = new Book(10, "Унесенные ветром", 670, "Маргарет Митчелл");
+    private Book second = new Book(11, "Мастер и Маргарита", 760, "Михаил Булгаков");
+    private Book third = new Book(12, "Грозовой перевал", 540, "Эмили Бронте");
+    private Book fourth = new Book(13, "Шерлок Холмс", 870, "Артур Конан Дойл");
+    private Book fifth = new Book(14, "Вино из одуванчиков", 450, "Рэй Брэдбери");
+    private Smartphone sixth = new Smartphone(15, "iPhone XR", 45000, "Apple");
+    private Smartphone seventh = new Smartphone(16, "Huawei P30", 31000, "Huawei");
+    private Smartphone eighth = new Smartphone(17, "Redmi Note 8T", 13000, "Redmi");
+
+    @Test
+    void add() {
+        Product[] returned = new Product[]{first};
+        doReturn(returned).when(repository).findAll();
+
+        manager.add(first);
+        Product[] expected = new Product[]{first};
+        Product[] actual = manager.getAll();
+
+        assertArrayEquals(expected, actual);
+        verify(repository).save(any());
+        verify(repository).findAll();
+
+    }
+
+    @Test
+    void searchBy() {
+    }
+
+    @Test
+    void matches() {
+    }
+}
