d74e98cba49cd6c98c4fbe7336a24cb3c676effb
FilStas <filstas23@gmail.com>
2020-08-17 19:42:54 +0400

    added Game

---
+++ src/main/java/ru/netology/domain/Game.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class Game extends Product {
+    public Game(int id, String name, int cost) {
+        super(id, name, cost);
+    }
+
+    public Game() {
+    }
+}
