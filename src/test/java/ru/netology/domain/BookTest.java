b107e9bfbe429c90ea46316c074a0016ed1093be
FilStas <filstas23@gmail.com>
2020-09-10 09:03:28 +0400

    modified BookTest

--- src/test/java/ru/netology/domain/BookTest.java
+++ src/test/java/ru/netology/domain/BookTest.java
@@ -2,8 +2,7 @@ package ru.netology.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BookTest {
 
@@ -17,14 +16,28 @@ class BookTest {
 
     @Test
     void shouldCheckIfTheBookMatchesTheSearchByAuthor() {
-        Product second = new Book(11, "Мастер и Маргарита", 760, "Михаил Булгаков");
-        Product third = new Book(12, "Грозовой перевал", 540, "Эмили Бронте");
-        Product fourth = new Book(13, "Шерлок Холмс", 870, "Артур Конан Дойл");
-        Product fifth = new Book(14, "Вино из одуванчиков", 450, "Рэй Брэдбери");
         Product first = new Book(10, "Унесенные ветром", 670, "Маргарет Митчелл");
 
         boolean actual = first.matches("Маргарет Митчелл");
 
         assertTrue(actual);
+    }
+
+    @Test
+    void shouldCheckIfTheBookMatchesTheSearchByName() {
+        Product first = new Book(10, "Унесенные ветром", 670, "Маргарет Митчелл");
+
+        boolean actual = first.matches("Унесенные ветром");
+
+        assertTrue(actual);
+    }
+
+    @Test
+    void shouldCheckSearchForBookIfThereIsAMistakeInName() {
+        Product first = new Book(10, "Унесенные ветром", 670, "Маргарет Митчелл");
+
+        boolean actual = first.matches("Унисенные ветром");
+
+        assertFalse(actual);
     }
 }
