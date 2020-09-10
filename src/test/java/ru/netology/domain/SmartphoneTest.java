adcb8c889ef40a4dc50a42cdb4a985a57517887e
FilStas <filstas23@gmail.com>
2020-09-10 09:03:48 +0400

    modified SmartphoneTest

--- src/test/java/ru/netology/domain/SmartphoneTest.java
+++ src/test/java/ru/netology/domain/SmartphoneTest.java
@@ -2,7 +2,7 @@ package ru.netology.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SmartphoneTest {
 
@@ -12,5 +12,32 @@ class SmartphoneTest {
         Product second = new Smartphone(15, "iPhone XR", 45000, "Apple");
 
         assertEquals(first, second);
+    }
+
+    @Test
+    void shouldCheckIfTheSmartphoneMatchesTheSearchByManufacturer() {
+        Smartphone seventh = new Smartphone(16, "Huawei P30", 31000, "Huawei");
+
+        boolean actual = seventh.matches("Huawei");
+
+        assertTrue(actual);
+    }
+
+    @Test
+    void shouldCheckIfTheSmartphoneMatchesTheSearchByName() {
+        Smartphone seventh = new Smartphone(16, "Huawei P30", 31000, "Huawei");
+
+        boolean actual = seventh.matches("Huawei P30");
+
+        assertTrue(actual);
+    }
+
+    @Test
+    void shouldChecksearchForSmartphoneIfThereIsAMistakeInName() {
+        Smartphone seventh = new Smartphone(16, "Huawei P30", 31000, "Huawei");
+
+        boolean actual = seventh.matches("Huawei P3");
+
+        assertFalse(actual);
     }
 }
